b4e43654f6574a032a51ec253c2380fcbab7d2f3
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 14:03:25 +0000

    fh

--- providers.tf
+++ providers.tf
@@ -1,13 +1,3 @@
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-    resource_group {
-      prevent_deletion_if_contains_resources = true
-    }
-  
-  }
 }
