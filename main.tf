bbe5966ad2b890a388e75c62fdbb7287d0563b11
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 10:17:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
 
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  
+  }
+}
+
+
+
+
+terraform {
+  required_version = ">= 1.4.0"    
+  backend "azurerm" {
+  }  
+}
