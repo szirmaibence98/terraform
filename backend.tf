0b4feb909367e8bb9dc95c3fa9b235fdd693ae1c
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 18:50:44 +0000

    gfj

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 
 terraform {
-  required_version = ">= 1.4.0"    
+  version = "~> 2.30"  
   backend "azurerm" {
     resource_group_name   = "terraform"
     storage_account_name  = "terraformszirmaidaimler"
