47d36c60d92dd33ec2cfe3877bdecde06d28dbb9
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 12:11:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ terraform {
     resource_group_name   = "terraform"
     storage_account_name  = "terraformszirmaidaimler"
     container_name        = "statefiles"
-    key                   = "terraform.tfstate"
+#    key                   = "terraform.tfstate"
   }
 }
 
