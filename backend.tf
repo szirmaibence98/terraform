e222767c4fdeed1a0c9738d15924de3a9c2a14b5
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 18:52:50 +0000

    fsf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,17 @@
-
-
 terraform {
-  version = "~> 2.30"  
+  required_version = ">= 0.12"  // Adjust this as necessary for your Terraform CLI version requirement
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.30"  // Correct way to specify the AzureRM provider version
+    }
+  }
+
   backend "azurerm" {
-    resource_group_name   = "terraform"
-    storage_account_name  = "terraformszirmaidaimler"
-    container_name        = "statefiles"
-#    key                   = "terraform.tfstate"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformszirmaidaimler"
+    container_name       = "statefiles"
+#    key                  = "terraform.tfstate"  // Uncomment or adjust as necessary
   }
 }
