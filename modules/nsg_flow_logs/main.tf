402cfab500e7a6214a43e4a113dfdf969fd5ee30
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 18:48:18 +0000

    asfasd

--- modules/nsg_flow_logs/main.tf
+++ modules/nsg_flow_logs/main.tf
@@ -1,10 +1,11 @@
 resource "azurerm_network_watcher_flow_log" "example" {
-  count               = length(var.nsg_ids)
-  name                = "example-flow-log-${count.index}" // Adjust this name as needed
+  // Assuming count or for_each is used to handle multiple NSG IDs
+  for_each             = toset(var.nsg_ids)
+  name                 = "flowLog-${each.key}" // Use each.key or each.value as appropriate
   network_watcher_name = var.network_watcher_name
   resource_group_name  = var.resource_group_name
 
-  network_security_group_id = var.nsg_ids[count.index]
+  network_security_group_id = each.value // Adjust based on your iteration method
   storage_account_id        = var.storage_account_id
   enabled                   = true
 
@@ -16,7 +17,7 @@ resource "azurerm_network_watcher_flow_log" "example" {
   traffic_analytics {
     enabled               = true
     workspace_id          = var.log_analytics_workspace_id
-    workspace_region      = var.location
+    workspace_region      = var.location // Ensure this is provided as a variable if required
     workspace_resource_id = var.log_analytics_workspace_resource_id
   }
 }
