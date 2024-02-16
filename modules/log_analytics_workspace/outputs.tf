a8a996cce6878db7c282f91e767122a467d496f2
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-16 19:12:30 +0000

    dsf

--- modules/log_analytics_workspace/outputs.tf
+++ modules/log_analytics_workspace/outputs.tf
@@ -1,6 +1,7 @@
 output "workspace_id" {
-  value = azurerm_log_analytics_workspace.workspace.id
+  value = azurerm_log_analytics_workspace.workspace.customer_id
 }
+
 
 output "workspace_primary_shared_key" {
   value     = azurerm_log_analytics_workspace.workspace.primary_shared_key
