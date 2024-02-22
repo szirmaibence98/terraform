3ec5f426efd28a6f36565f984c173da02ebfbf89
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-22 21:23:21 +0100

    sdf

--- modules/log_analytics_workspace/outputs.tf
+++ modules/log_analytics_workspace/outputs.tf
@@ -2,9 +2,9 @@ output "workspace_id" {
   value = azurerm_log_analytics_workspace.example.workspace_id
 }
 
-#output "workspace_resource_id" {
-#  value = azurerm_log_analytics_workspace.example.id
-#}
+output "workspace_resource_id" {
+  value = azurerm_log_analytics_workspace.example.id
+}
 
 
 output "workspace_primary_shared_key" {
@@ -12,6 +12,3 @@ output "workspace_primary_shared_key" {
   sensitive = true
 }
 
-output "workspace_id" {
-  value = azurerm_log_analytics_workspace.example.id
-}
