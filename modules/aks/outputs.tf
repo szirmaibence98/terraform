ed4afcad78186a48fcb0666a999e1da92f932fe6
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-27 22:28:29 +0100

    sdf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -7,3 +7,7 @@ output "cluster_name" {
   description = "The name of the AKS cluster."
   value       = var.cluster_name
 }
+
+output "principal_id" {
+  value = azurerm_kubernetes_cluster.k8s.identity[0].principal_id
+}
