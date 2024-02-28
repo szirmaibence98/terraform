cab588d39ab30fc4e25547e0ba905fd8593c22cb
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-28 09:13:43 +0100

    dfg

--- modules/monitor-alert-prometheus-rule-group/main.tf
+++ modules/monitor-alert-prometheus-rule-group/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_monitor_alert_prometheus_rule_group" "example" {
   location              = var.location
   resource_group_name   = var.resource_group_name
   cluster_name          = var.cluster_name
-  scopes                = [var.kubernetes_cluster_id]
+  scopes                = [var.workspace_id, var.kubernetes_cluster_id]
 
   dynamic "rule" {
     for_each = [for r in var.rules : r if r.record != null]
