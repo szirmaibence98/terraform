9d59c8975125c45959aafe057f20d4c2e3bd431d
szirmaibence98 <40829338+szirmaibence98@users.noreply.github.com>
2024-02-28 09:00:45 +0100

    sdsd

--- main.tf
+++ main.tf
@@ -232,74 +232,6 @@ module "data_collection_rule_association" {
 }
 
 
-module "prometheus_rule_group" {
-  source                = "./modules/prometheus_rule_group"
-  name                  = "ruleseta"
-  cluster_name          = "myCluster"
-  location              = var.location
-  resource_group_name   = module.resource_group.name
-  workspace_id          = module.monitor_workspace.workspace_id
-  kubernetes_cluster_id = module.aks.cluster_id
-
-
-  rules = [
-    {
-      enabled    = true,
-      record     = "instance:node_num_cpu:sum",
-      expression = "count without (cpu, mode) (node_cpu_seconds_total{job=\"node\",mode=\"idle\"})"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_cpu_utilisation:rate5m",
-      expression = "1 - avg without (cpu) (sum without (mode) (rate(node_cpu_seconds_total{job=\"node\", mode=~\"idle|iowait|steal\"}[5m])))"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_load1_per_cpu:ratio",
-      expression = "(node_load1{job=\"node\"}/instance:node_num_cpu:sum{job=\"node\"})"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_memory_utilisation:ratio",
-      expression = "1 - ((node_memory_MemAvailable_bytes{job=\"node\"} or (node_memory_Buffers_bytes{job=\"node\"} + node_memory_Cached_bytes{job=\"node\"} + node_memory_MemFree_bytes{job=\"node\"} + node_memory_Slab_bytes{job=\"node\"}))/node_memory_MemTotal_bytes{job=\"node\"})"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_vmstat_pgmajfault:rate5m",
-      expression = "rate(node_vmstat_pgmajfault{job=\"node\"}[5m])"
-    },
-    {
-      enabled    = true,
-      record     = "instance_device:node_disk_io_time_seconds:rate5m",
-      expression = "rate(node_disk_io_time_seconds_total{job=\"node\", device!=\"\"}[5m])"
-    },
-    {
-      enabled    = true,
-      record     = "instance_device:node_disk_io_time_weighted_seconds:rate5m",
-      expression = "rate(node_disk_io_time_weighted_seconds_total{job=\"node\", device!=\"\"}[5m])"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_network_receive_bytes_excluding_lo:rate5m",
-      expression = "sum without (device) (rate(node_network_receive_bytes_total{job=\"node\", device!=\"lo\"}[5m]))"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_network_transmit_bytes_excluding_lo:rate5m",
-      expression = "sum without (device) (rate(node_network_transmit_bytes_total{job=\"node\", device!=\"lo\"}[5m]))"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_network_receive_drop_excluding_lo:rate5m",
-      expression = "sum without (device) (rate(node_network_receive_drop_total{job=\"node\", device!=\"lo\"}[5m]))"
-    },
-    {
-      enabled    = true,
-      record     = "instance:node_network_transmit_drop_excluding_lo:rate5m",
-      expression = "sum without (device) (rate(node_network_transmit_drop_total{job=\"node\", device!=\"lo\"}[5m]))"
-    }
-  ]
-}
 
 
 
@@ -324,6 +256,7 @@ module "prometheus_rule_group" {
 #  role_definition_name_or_id  = "Grafana Admin"
 #  principal_id                = "592ce1c9-046b-45a7-ae5f-07b565cec71c"
 #}
+
 
 
 
